22c850e7cde8ccaf7e7f8ab3dcd958a316b18db2
Benedict1210 <167989768+Benedict1210@users.noreply.github.com>
2025-03-05 20:26:39 +0700

    Sprites and player control

--- Assets/player/Scripts/PlayerController.cs
+++ Assets/player/Scripts/PlayerController.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float jumpForce = 15f;
+    [SerializeField] private float moveSpeed = 5f, jumpForce = 15f;
+    [SerializeField] private int extraJumpCount = 2;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Transform groundCheck;
 
-    //private float jumpsLeft;
-    //private float lastJump;
-    private Animator animator;
+    //private Animator animator;
     private bool isGrounded;
+    private float jumps;
     private Rigidbody2D rb;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        //animator = GetComponent<Animator>();
     }
     void Start()
     {
@@ -29,15 +28,18 @@ public class PlayerController : MonoBehaviour
     {
         HandleMovement();
         HandleJump();
-        UpdateAnimation();
+        //UpdateAnimation();
     }
     private void HandleMovement()
     {
         float moveInput = Input.GetAxis("Horizontal");
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        if (isGrounded) rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
 
         if (moveInput > 0) transform.localScale = new Vector3(1, 1, 1);
         else if (moveInput < 0) transform.localScale = new Vector3(-1, 1, 1);
+
+        Debug.DrawLine(transform.position, transform.position + new Vector3(rb.linearVelocity.x, rb.linearVelocity.y, 0));
 
         if (transform.position.y < -100)
         {
@@ -49,8 +51,14 @@ public class PlayerController : MonoBehaviour
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (isGrounded)
         {
+            jumps = extraJumpCount;
+        }
+
+        if (Input.GetButtonDown("Jump") && (isGrounded || (jumps > 0)))
+        {
+            jumps -= 1;
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
     }
@@ -59,7 +67,7 @@ public class PlayerController : MonoBehaviour
         bool isRunning = Mathf.Abs(rb.linearVelocity.x) > .1f;
         bool isJumping = !isGrounded;
 
-        animator.SetBool("isRunning", isRunning);
-        animator.SetBool("isJumping", isJumping);
+        //animator.SetBool("isRunning", isRunning);
+        //animator.SetBool("isJumping", isJumping);
     }
 }
